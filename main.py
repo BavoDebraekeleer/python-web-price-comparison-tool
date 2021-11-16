ec5e86d7789028bf8393835c907532fa674ba9ca
Bavo Debraekeleer <bavo_debraekeleer@msn.com>
2021-11-16 17:42:46 +0100

    Project start

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+url = ""
+headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"}
